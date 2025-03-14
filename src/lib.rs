bf21fa4a327240be38d7d61c0f4013f905868449
left-TTC <leftear1321@gmail.com>
2025-03-14 09:48:17 +0800

    0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use proc_macro::TokenStream;
+use quote::quote;
+use anchor_lang::solana_program::pubkey::Pubkey;
+use syn::{parse_macro_input, LitStr};
+use std::str::FromStr;
+
+
+#[proc_macro]
+pub fn declare_central_state(input: TokenStream) -> TokenStream {
+    let program_id_str = parse_macro_input!(input as LitStr);
+    let program_id = program_id_str.value();
+
+    let pubkey = Pubkey::from_str(&program_id).expect("Invalid program ID");
+    let pubkey_bytes = pubkey.to_bytes();
+
+    let (central_state, central_state_nonce) = Pubkey::find_program_address(&[&pubkey_bytes], &pubkey);
+    let central_state_bytes = central_state.to_bytes();
+
+    let expanded = quote! {
+        pub mod central_state {
+            use anchor_lang::solana_program::pubkey::Pubkey;
+
+            pub static KEY_BYTES: [u8; 32] = [#(#central_state_bytes),*];
+            pub static KEY: Pubkey = Pubkey::new_from_array(KEY_BYTES);
+            pub static NONCE: u8 = #central_state_nonce;
+            pub static SIGNER_SEEDS: [&'static [u8]; 2] = [&super::ID_BYTES, &[NONCE]];
+        }
+
+        pub static ID_BYTES: [u8; 32] = [#(#pubkey_bytes),*];
+    };
+
+    TokenStream::from(expanded)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
